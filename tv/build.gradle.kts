f42505c2ed6058de22366c8bb8f0cd9ba12aa63e
aliyun3636305658 <huxu7755@gmail.com>
2025-06-13 12:19:16 +0800

    修复release签名配置

--- tv/build.gradle.kts
+++ tv/build.gradle.kts
@@ -11,6 +11,14 @@ plugins {
 
 
 android {
+    signingConfigs {
+        create("release") {
+            storeFile = file(System.getenv("STORE_FILE") ?: "release.keystore")
+            storePassword = System.getenv("STORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
     @Suppress("UNCHECKED_CAST")
     apply(extra["appConfig"] as BaseAppModuleExtension.() -> Unit)
 
